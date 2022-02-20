06e5cf4d88c8a676b26bdd2bd3685c4f6d40e449
Celia <celiamdona@gmail.com>
2022-02-20 15:36:56 -0500

    added sections of readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+
+  ## Installation
+
+  ## Usage
+
+   (assets/images/screenshot.png)
+    
+  ## License
+
+  ## Contributing
+  
+  If you like to contribute to this application, please refer to the [Contributor Covenant](https://www.contributor-covenant.org/) guidelines.
+
+  ## Tests
+
+  ## Questions
+
+  
 
 `;
 }
